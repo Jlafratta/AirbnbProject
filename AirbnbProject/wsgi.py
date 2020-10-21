458a5c9dfffc1b1e344cbd47747d6083caab2d0a
Jlafratta <julian.lafratta@gmail.com>
2020-10-21 16:59:51 -0300

    base project files

---
+++ AirbnbProject/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for AirbnbProject project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AirbnbProject.settings')
+
+application = get_wsgi_application()
