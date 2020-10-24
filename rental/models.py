e1a16e05ce47cded9013ab5c7f0ca1073fd4e41c
Jlafratta <julian.lafratta@gmail.com>
2020-10-24 03:40:01 -0300

    models mapped

--- rental/models.py
+++ rental/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+
+class City(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Property(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+    capacity = models.IntegerField(default=0)
+    daily_price = models.FloatField(default=0.0)
+    # FK que vincula con City
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
+
+
+class Reservation(models.Model):
+    total_price = models.FloatField(default=0.0)
+
+
+class ReservationDates(models.Model):
+    date = models.DateField
+    property = models.ForeignKey(Property, on_delete=models.CASCADE)
+    reservation = models.ForeignKey(Reservation, on_delete=models.CASCADE)
+
+
